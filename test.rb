fa4904575cacb59d46fb16d38ab69d01885d6412
Makoto Inoue <inouemak@googlemail.com>
2011-03-21 01:49:23 +0000

    Changed to test per prefecture

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ require 'rack'
 
 Capybara.app = Rack::Builder.new
 Capybara.app_host = "http://www.e-stat.go.jp/"
-Capybara.default_wait_time = 30
+Capybara.default_wait_time = 130
 Capybara.register_driver :selenium do |app|
   # Capybara::Driver::Selenium.new(app, :browser => :chrome)
   Capybara::Driver::Selenium.new(app, :browser => :firefox, :profile => "e-stat")
@@ -20,7 +20,13 @@ Spec::Runner.configure do |config|
 end
 
 describe "the signup process", :type => :request do
-  it "signs me in" do
+  Prefs = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+  
+  Prefs[0..3].each do |pref|
+    it "#{pref} should be downloaded" do
+    data = []
+    
+    p "Start time: #{Time.now}"
     visit '/SG1/estat/toukeiChiri.do?method=init'
     page.driver.browser.execute_script %Q{
       document.links[7].removeAttribute('target');
@@ -29,9 +35,10 @@ describe "the signup process", :type => :request do
     select('平成１７年国勢調査（小地域）　2005/10/01')
     check('d1_tT000051')
     click_button("　次　へ　")
-    select('岩手県')
+    puts "Prefecture : #{pref} : #{Time.now}"
+    select(pref)
     all('select[name="selectedCities"] option')[0..2].each do |node|
-      p node.text
+      p "Choosing city : #{node.text} : #{Time.now}"
       select(node.text)
       #  Gets  Element not found in the cache error
       # node.select_option unless node.selected?
@@ -41,9 +48,8 @@ describe "the signup process", :type => :request do
     all('table tbody')[4..4].each do |table|
       table.all('a').each do |a|
         unless a.text == "定義書"
-          p a.text
+          p "City : #{a.text} : #{Time.now}"
           a.click
-          
           # sleep 20
           # Not working. Maybe beacuse this is browser dialog, rather than javascript popup.
           # page.driver.browser.within_popup do
@@ -54,5 +60,9 @@ describe "the signup process", :type => :request do
         end
       end
     end
+    p data
+  p "End time: #{Time.now}"
   end
+  end
+
 end
