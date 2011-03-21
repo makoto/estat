91c67ce7b73246d1698dbab28e5fb77c84099830
Makoto Inoue <inouemak@googlemail.com>
2011-03-21 01:50:13 +0000

    Include all prefs and cities

--- test.rb
+++ test.rb
@@ -22,7 +22,7 @@ end
 describe "the signup process", :type => :request do
   Prefs = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
   
-  Prefs[0..3].each do |pref|
+  Prefs.each do |pref|
     it "#{pref} should be downloaded" do
     data = []
     
@@ -37,7 +37,7 @@ describe "the signup process", :type => :request do
     click_button("　次　へ　")
     puts "Prefecture : #{pref} : #{Time.now}"
     select(pref)
-    all('select[name="selectedCities"] option')[0..2].each do |node|
+    all('select[name="selectedCities"] option').each do |node|
       p "Choosing city : #{node.text} : #{Time.now}"
       select(node.text)
       #  Gets  Element not found in the cache error
